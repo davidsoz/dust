b9728b6e233f2923a280394024c5fc4f67a84cda
David <davidsozashvili@gmail.com>
2024-03-22 22:24:07 +0400

    make button look nice

--- src/styled.js
+++ src/styled.js
@@ -6,4 +6,23 @@ export const Wrapper = styled.div`
   align-items: center;
   flex-direction: column;
   gap: 3px;
+  button {
+    padding: 10px 20px;
+    background-color: dodgerblue;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    font-size: 16px;
+    cursor: pointer;
+    outline: none;
+    transition: background-color 0.3s ease-in-out;
+  }
+
+  button:hover {
+    background-color: #007acc;
+  }
+
+  button:active {
+    background-color: #005f99;
+  }
 `;
